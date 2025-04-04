f420fb845435a66326ca98350a0f9c16a0a05d1b
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-05 03:00:37 +0530

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { BrowserRouter as  Router, Routes, Route } from 'react-router'
+import PublicLayout from "./layouts/public-layout"
+import HomePage from "./pages/HomePage"
+import { AuthenticatedLayout } from './layouts/auth-layout'
+import { SignIn } from './pages/Signin'
+import { SignUp } from './pages/Signup'
+import { ProtectedLayout } from './layouts/protected-layout'
+import MainLayout from './layouts/main-layout'
+
+
 function App() {
 
   return (
-    <div className="h-100 w-full flex justify-center p-4">
-      hello
-    </div>
+    <Router>
+      <Routes>
+        {/* public route */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* authenticated route */}
+        <Route element={<AuthenticatedLayout />}>
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Route>
+
+        {/* protected route */}
+        <Route element={
+          <ProtectedLayout> 
+            <MainLayout /> 
+          </ProtectedLayout>
+        }>
+          {/* all the protected routes */}
+        </Route>
+      </Routes>
+    </Router>
+    
   )
 }
+
 
 export default App
