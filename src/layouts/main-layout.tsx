a1f4c6932013dd5068e8327eb3ee32561806ac30
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-08 00:51:06 +0530

    update imports

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Header from "@/components/header/Header"
 import { Container } from "@/components/Container"
 import { Outlet } from "react-router"
 
--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,5 +1,5 @@
 import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Header from "@/components/header/Header"
 import { Outlet } from "react-router"
 
 const PublicLayout = () => {
