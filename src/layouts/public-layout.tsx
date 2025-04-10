872cce39e4ed76d50d995cfc3f724e7460a9f108
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-10 14:17:12 +0530

    add auth handler in public layout

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/components/Footer"
 import Header from "@/components/header/Header"
+import { AuthHandler } from "@/handler/AuthHandler"
 import { Outlet } from "react-router"
 
 const PublicLayout = () => {
   return (
     <div>
         {/* handler to store the user data */}
+        <AuthHandler />
+
         <Header />
 
         <Outlet />
