5dbc49ffd27eb9ef5861091b6bf5d56e542356cc
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-10 14:17:22 +0530

    add navlink

---
+++ src/lib/helpers.ts
@@ -0,0 +1,14 @@
+export const navlinks = [
+    {
+        label: "Home",
+        href: "/"
+    },
+    {
+        label: "Features",
+        href: "/features"
+    },
+    {
+        label: "About Us",
+        href: "/about"
+    },
+]
