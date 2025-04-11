856ac8e48227a7ea3794d2f94a33ce033e7dd8b8
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-12 02:52:45 +0530

    add customfirebasetokenLink

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+export const customFirebaseTokenLink = import.meta.env.VITE_CUSTOMFIREBASE_TOKEN_LINK;
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
