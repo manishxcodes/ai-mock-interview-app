38ac6bba25d41c7c00cbfffd11bd389ffbddb224
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-12 02:53:29 +0530

    style: increase gap

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -26,7 +26,7 @@ const Header =  () => {
 
           
         {/* cta */}
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-4">
           <Darktheme />
           <ProfileContainer />
           {/* <HamburgerMenu /> */}
