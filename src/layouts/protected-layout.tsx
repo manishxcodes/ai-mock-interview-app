c61aa5ecd08a15de4e1b74670f252c69a6e4ba5a
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-15 21:03:57 +0530

    fix issue: redirect to signin page if user is not signin

--- src/layouts/protected-layout.tsx
+++ src/layouts/protected-layout.tsx
@@ -1,15 +1,17 @@
 import { Loader } from "@/components/Loader";
 import { useAuth } from "@clerk/clerk-react"
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router";
+
 
 export const ProtectedLayout = ({children}: {children: React.ReactNode}) => {
     const {isLoaded, isSignedIn} = useAuth();
+    const navigate = useNavigate()
     
     // check if children is loaded
     if(!isLoaded) return <Loader />
 
     // check if user is signed-in
-    if(!isSignedIn) <Navigate to={"/signin"} replace/>
+    if(!isSignedIn) navigate('/signin');
 
     return children;
 }
