feef32d989daf7dcd3388fbb7b26c25d1ac3cdff
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-16 17:53:45 +0530

    add interview and form types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
-
+import { z } from "zod"
 export interface User {
     id: string;
     name: string;
@@ -8,3 +8,32 @@ export interface User {
     createdAt: Timestamp | FieldValue;
     updatedAt: Timestamp | FieldValue;
 }
+
+export interface Interview {
+    id: string;
+    position: string;
+    description: string;
+    experience: number;
+    userId: string;
+    techStack: string;
+    questions: {
+        question: string;
+        answer: string;
+    }[];
+    createdAt: Timestamp;
+    updatedAt: Timestamp;
+}
+
+export const formSchema = z.object({
+    position: z
+      .string()
+      .min(1, "Position is required")
+      .max(100, "Position must be 100 characters or less"),
+    description: z.string().min(10, "Description is required"),
+    experience: z.coerce
+      .number()
+      .min(0, "Experience cannot be empty or negative"),
+    techStack: z.string().min(1, "Tech stack must be at least a character"),
+  });
+  
+  export type FormData = z.infer<typeof formSchema>;
