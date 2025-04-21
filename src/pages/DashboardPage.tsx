b7a551d7ce30fd7cd20d0f430db58560eb02319a
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-21 21:23:45 +0530

    add fetch interview  from function database

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,33 +1,89 @@
-import { InterviewForm } from "@/components/interview-form"
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/Heading"
+import { db } from "@/config/firebase.config"
+import { Interview } from "@/types"
 import { useUser } from "@clerk/clerk-react"
 import { Separator } from "@radix-ui/react-separator"
-import { Plus } from "lucide-react"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { Loader, Plus } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router"
+import { Container } from "@/components/Container"
+import InterviewCard from "@/components/interview-card"
 
 export const DashboardPage = () => {
-  const { user } = useUser();
+  const user = useUser();
+  const userId = user?.user?.id
+  
+  const [loading, setLoading] = useState(false);
+  const [interviews, setInterviews] = useState<Interview[]>([]);
+
+  useEffect(() => {
+    const fetchInterviews = async () => {
+      if (!userId) return;
+
+      setLoading(true);
+
+      try {
+        const interviewsRef = collection(db, "interviews");
+
+        // Query interviews where userId matches the logged-in user
+        const q = query(interviewsRef, where("userId", "==", userId));
+        const querySnapshot = await getDocs(q);
+
+        console.log("querySnapshot: ", querySnapshot.docs[0].data());
+
+      // Process the data correctly
+      const data: Interview[] = querySnapshot.docs.map(doc => ({
+        id: doc.id, 
+        ...(doc.data() as Omit<Interview, 'id'>),
+      }));
+
+      console.log("questions: ", data);
+      
+      setInterviews(data);
+      } catch (err) {
+        console.error("Error fetching interviews:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchInterviews();
+  }, [userId]);
 
   return (
     <>
       <div className="w-full flex items-center justify-between">
 
         {/* headings */}
-        <Heading title={`Welcome, ${user?.firstName}`} description={"Create and start your mock interview with AI"} />
+        <Heading title={`Welcome, ${user?.user?.firstName}`} description={"Create and start your mock interview with AI"} />
 
         {/* content */}
-        <Link to={":interviewId"}>
+        <Link to={"create"}>
           <Button size={"sm"} >
             <Plus /> Add New
           </Button>
         </Link> 
       </div>
 
-      
-      <Separator className="my-8" />
+      <Separator className="my-10" />
 
-      <InterviewForm />
+      {  loading && 
+        <Container >
+          <div className="w-full h-96 flex justify-center items-center">
+            < Loader />
+          </div>
+        </Container>
+      }
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {
+          interviews.map((interview) => (
+            <InterviewCard key={interview.id} interview={interview} />
+          ))
+        }
+      </div>
     </>
   )
 }
