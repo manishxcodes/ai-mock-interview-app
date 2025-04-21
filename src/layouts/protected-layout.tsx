4d3b10a6010305b9db9ebd262f6e388aa8f02a58
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-04-21 21:23:01 +0530

    fix: signin with firebase in protected route

--- src/layouts/protected-layout.tsx
+++ src/layouts/protected-layout.tsx
@@ -1,14 +1,24 @@
 import { Loader } from "@/components/Loader";
 import { useAuth } from "@clerk/clerk-react"
+import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
-
 
 export const ProtectedLayout = ({children}: {children: React.ReactNode}) => {
     const {isLoaded, isSignedIn} = useAuth();
-    const navigate = useNavigate()
-    
-    // check if children is loaded
-    if(!isLoaded) return <Loader />
+    const navigate = useNavigate();
+    const firebaseAuth = getAuth();
+
+    // signin with firebase
+    useEffect(() => {
+        const firebaseSignin = async () => {
+            const firebaseToken = localStorage.getItem("firebaseToken")!;
+
+            const credentials = await signInWithCustomToken(firebaseAuth, firebaseToken);
+            console.log("Firebase credentials:", credentials.user.uid);
+        }
+        firebaseSignin();
+    }, [])
 
     // check if user is signed-in
     if(!isSignedIn) navigate('/signin');
