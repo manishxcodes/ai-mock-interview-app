529c59bdd6bfca6c6713b6a87356648538542f28
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-01 01:30:18 +0530

    add user-answer type

--- src/types/index.ts
+++ src/types/index.ts
@@ -24,6 +24,23 @@ export interface Interview {
     updatedAt: Timestamp;
 }
 
+export interface UserAnswer {
+  question: string;
+  correct_answer: string;
+  user_answer: string;
+  feedback: string;
+  rating: number;
+}
+
+export interface UserAnswers {
+  userId: string;
+  interviewId: string;
+  answers: UserAnswer[];
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+}
+
+
 export const formSchema = z.object({
     position: z
       .string()
