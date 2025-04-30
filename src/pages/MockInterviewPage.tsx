f5cf710008a15718a05ca98bc671e13d3fae4888
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-01 01:31:03 +0530

    add mock interview page

---
+++ src/pages/MockInterviewPage.tsx
@@ -0,0 +1,48 @@
+import { Loader } from "@/components/Loader";
+import { QuestionForm } from "@/components/question-form";
+import { Button } from "@/components/ui/button";
+import { useInterview } from "@/hooks/useInterview";
+import { Link, useNavigate, useParams } from "react-router"
+import { toast } from "sonner";
+
+export const MockInterviewPage = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const { interview, loading, error } = useInterview(id);
+
+    if(!id ) {
+        toast("Interview not found");
+        navigate("/generate");
+    }
+
+    if(loading) return <Loader />
+
+    if(error ) {
+        toast.error("Something went wrong!", {description: "Please try againg"});
+        navigate("/generate");
+    }
+
+    if (!interview) {
+      return (
+        <div className="flex flex-col items-center justify-center w-full h-[50vh] text-center gap-4">
+          <h2 className="text-xl font-semibold">No interview data found</h2>
+          <Link to={"/generate"}>
+            <Button>Go Back and retry </Button>
+          </Link>
+        </div>
+      )
+    }
+
+    console.log(interview);
+    return (
+        <div className="w-full">
+           { interview?.questions && interview.questions.length > 0 && (
+                <div>
+                    <QuestionForm questions={interview?.questions} />
+                </div>
+            )
+           }
+        </div>
+    )
+}
