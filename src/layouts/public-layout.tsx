caf4bc74f905f0c2199fc0f047c5f76e4fad53f5
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-03 02:37:38 +0530

    fix import for footer

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer"
+import { Footer } from "@/components/Footer"
 import Header from "@/components/header/Header"
 import { AuthHandler } from "@/handler/AuthHandler"
 import { Outlet } from "react-router"
