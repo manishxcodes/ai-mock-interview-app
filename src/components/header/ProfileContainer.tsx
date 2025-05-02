a296327785a86baad8cf27c0bd034a119206598c
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-03 02:38:00 +0530

    remove unused variable

--- src/components/header/ProfileContainer.tsx
+++ src/components/header/ProfileContainer.tsx
@@ -1,18 +1,9 @@
 import { useAuth, UserButton } from "@clerk/clerk-react"
-import { Loader } from "../Loader"
 import { Button } from "../ui/button"
 import { Link } from "react-router"
 
 export const ProfileContainer = () => {
-    const {isSignedIn, isLoaded} = useAuth();
-
-    // if(!isLoaded) {
-    //     return (
-    //         <div className="flex items-center">
-    //             <Loader className="min-w-4 miin-h-4 animate-spin " />
-    //         </div>
-    //     )
-    // }
+    const {isSignedIn,} = useAuth();
 
     return (
         <div className="flex items-center">
