4d616d282e7b4c878310adc88ece208b4b4b9f0c
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-03 01:43:40 +0530

    add all routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import { SignUp } from './pages/SignUp'
 import { SignIn } from './pages/SignIn'
 import { ProtectedLayout } from './layouts/protected-layout'
 import MainLayout from './layouts/main-layout'
+import { DashboardPage } from './pages/DashboardPage'
+import { CreatePage } from './pages/CreatePage'
+import { InterviewLoadPage } from './pages/InterviewLoadPage'
+import { Generate } from './components/Generate'
+import { MockInterviewPage } from './pages/MockInterviewPage'
+import { InterviewDataProvider } from './context/InterviewDataContext'
+import { Feedback } from './pages/Feedback'
 
 
 function App() {
@@ -20,8 +27,8 @@ function App() {
 
         {/* authenticated route */}
         <Route element={<AuthenticatedLayout />}>
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup/*' element={<SignUp />} />
+          <Route path='/signin/*' element={<SignIn />} />
         </Route>
 
         {/* protected route */}
@@ -31,6 +38,17 @@ function App() {
           </ProtectedLayout>
         }>
           {/* all the protected routes */}
+          <Route path='/generate' element={
+            <InterviewDataProvider>
+              <Generate />
+            </InterviewDataProvider>} >
+            <Route index element={<DashboardPage />} />
+            <Route path='create' element={<CreatePage />} />
+            <Route path='create/:id' element={<CreatePage />} />
+            <Route path='interview/:id' element={<InterviewLoadPage />} />
+            <Route path='interview/:id/start' element={<MockInterviewPage />} />
+            <Route path="feedback/:id" element={<Feedback />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
