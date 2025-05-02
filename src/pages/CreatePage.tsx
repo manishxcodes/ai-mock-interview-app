e97f54e9e06e8c848e09f901a1b278bef9ba83ae
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-03 01:47:11 +0530

    add conditional alert message

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/components/Container";
 import { useParams } from "react-router"
 import { InterviewForm } from "@/components/interview-form";
 import { useInterview } from "@/hooks/useInterview";
+import { AlertCard } from "@/components/alert-card";
 
 export const CreatePage = () => {
   const { id } = useParams<{id: string}>();
@@ -11,6 +12,18 @@ export const CreatePage = () => {
 
   return (
     <Container>
+
+      {id && (
+        <AlertCard  
+          title={"Warning: Editing Interview Will Overwrite Answers"} 
+          descriptions={
+            [
+              "Editing this interview will delete any previously saved answers.",
+              "To preserve your original answers, consider creating a new interview instead of editing this one."
+            ]
+          } 
+        />
+      )}
 
       <div className="my-4 flex-col w-full">
         <InterviewForm initialData={interview} />
