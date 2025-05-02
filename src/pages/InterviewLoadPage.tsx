2f5fea22742f41962a1f85c3f05f39969bf2afed
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-03 00:22:04 +0530

    add loader and start interview button

--- src/pages/InterviewLoadPage.tsx
+++ src/pages/InterviewLoadPage.tsx
@@ -1,11 +1,11 @@
 import { AlertCard } from "@/components/alert-card";
 import InterviewCard from "@/components/interview-card";
-import { Loader } from "@/components/Loader";
 import { Button } from "@/components/ui/button";
-import { useInterview } from "@/hooks/useInterview";
-import { ListRestart } from "lucide-react";
 import { Link, useNavigate, useParams } from "react-router"
 import { toast } from "sonner";
+import { useInterview } from "@/hooks/useInterview";
+import { Loader } from "lucide-react";
+
 
 export const InterviewLoadPage = () => {
     const { id } = useParams();
@@ -18,7 +18,11 @@ export const InterviewLoadPage = () => {
         navigate("/generate");
     }
 
-    if(loading) return <Loader />
+    if(loading) return (
+        <div className="w-full h-[80vh] flex items-center justify-center">
+          <Loader className="animate-spin" />
+        </div>
+      )
 
     if(error ) {
         toast.error("Something went wrong!", {description: "Please try againg"});
@@ -30,7 +34,7 @@ export const InterviewLoadPage = () => {
         <div className="flex flex-col items-center justify-center w-full h-[50vh] text-center gap-4">
           <h2 className="text-xl font-semibold">No interview data found</h2>
           <Link to={"/generate"}>
-            <Button>Go Back and retry <ListRestart /> </Button>
+            <Button>Go Back and retry </Button>
           </Link>
         </div>
       )
@@ -38,15 +42,26 @@ export const InterviewLoadPage = () => {
 
     console.log("interview: ", interview?.position)
   return (
-    <div className="flex flex-col w-full gap-8 py-6">
-        <AlertCard  
-        title={"Importance Information"} 
-        descriptions={
-          ['Please enable your webcam and microphone to start the mock interview. The interview consists of 8 questions. You’llreceive a personalized report based on your responses at the end.',
-           'Note:Your video is never recorded. You can disable your webcam at anytime.']} 
-        />
-        <div className="flex items-center justify-between w-full gap-2">
-          <InterviewCard interview={interview} variant={"full"} />
+    <div className="flex flex-col w-full gap-8">
+        <Link to={'/generate'}>
+          <Button className="underline" variant={'link'}>Go to dashboard</Button>
+        </Link>
+        <div>
+          <AlertCard  
+          title={"Importance Information"} 
+          descriptions={
+            ['Note:Your video is never recorded. You can disable your webcam at anytime.']
+            } 
+          />
+          <div className="flex items-center justify-between w-full gap-2">
+            <InterviewCard interview={interview} variant={"full"} />
+          </div>
+        </div>
+
+        <div className="flex items-start justify-center h-[45vh]">
+          <Link to={`start`}>
+            <Button variant={"default"}>Start Interview</Button>
+          </Link>
         </div>
     </div>
   )
